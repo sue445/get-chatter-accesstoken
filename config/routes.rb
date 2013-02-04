3686ba0c9fb7bab7886ad42392cacd663b688eaf
sue445 <sue445@sue445.net>
2013-02-05 00:24:50 +0900

    be rails g controller home index callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GetChatterAccesstoken::Application.routes.draw do
+  match 'callback' => 'home#callback'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ GetChatterAccesstoken::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/views/home/callback.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "home/callback.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
